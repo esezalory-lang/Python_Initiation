1f1d8ff87401a39673ead95271ecd9ec8bf9f5c7
Elliot Sezalory <esezalor@c2r1p10.42vienna.com>
2026-05-28 21:05:25 +0200

    90% done with ex4

--- 42Next_Python/python_module_03/ex4/ft_inventory_system.py
+++ 42Next_Python/python_module_03/ex4/ft_inventory_system.py
@@ -6,13 +6,49 @@ import sys
 def inventory_parser(input: list) -> dict:
     inventory = {}
     for pair in input:
-        key, value = pair.split(":")
-        inventory[key] = value
-    print(inventory)
+        split_worked = True
+        try:
+            key, value = pair.split(":")
+            if key in inventory.keys():
+                print(f"Redundant item '{key}' - discarding")
+                continue
+        except ValueError:
+            split_worked = False
+            print(f"Error - invalid parameter '{pair}'")
+        try:
+            if split_worked is False:
+                continue
+            inventory[key] = int(value)
+        except ValueError as v:
+            print(f"Quantity error for '{key}': {v}")
+    return inventory
 
 
+def inventory_analysis(inventory: dict) -> None:
+    total = sum(inventory.values())
+    max_q = 0
+    max_item = ""
+    min_q = 0
+    min_item = ""
+    for item, quantity in inventory.items():
+        if quantity > max_q:
+            max_q = quantity
+            max_item = item
+        if min_q == 0:
+            min_q = quantity
+            min_item = item
+        if min_q > quantity:
+            min_q = quantity
+            min_item = item
+        print(f"Item {item} represents {round((quantity/total)*100, 1)}%")
+    print(f"Item most abundant: {max_item} with quantity {max_q}")
+    print(f"Item least abundant: {min_item} with quantity {min_q}")
 
 
 if __name__ == "__main__":
-    print("=== Inventory System Analysis ===\n")
-    inventory_parser(sys.argv[1::])
+    print("=== Inventory System Analysis ===")
+    inv = inventory_parser(sys.argv[1::])
+    print(f"Got inventory: {inv}")
+    print(f"Item list: {inv.keys()}")
+    print(f"Total quantity of the {len(inv.keys())} items: {sum(inv.values())}")
+    inventory_analysis(inv)
